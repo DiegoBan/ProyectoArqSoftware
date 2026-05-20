df6f5be6f4afbbdbf1ec2cf31737f4c3d08fa8e6
DiegoBan <bandadiego452@gmail.com>
2026-05-20 12:30:47 -0400

    creacion base de datos + docker-compose inicial

---
+++ DB/01_init.sql
@@ -0,0 +1,81 @@
+CREATE TABLE IF NOT EXISTS usuarios (
+    id SERIAL PRIMARY KEY,
+    rut VARCHAR(12) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    nombre VARCHAR(80) NOT NULL,
+    apellido VARCHAR(80) NOT NULL,
+    rol NUMERIC(1, 0) NOT NULL,
+    telefono VARCHAR(20) NOT NULL,
+    Fecha_nacimiento DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS clientes (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    rut_empresa NUMERIC(9, 0) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS productos (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    familia VARCHAR(80) NOT NULL,
+    subfamilia VARCHAR(80),
+    descripcion TEXT NOT NULL,
+    PN VARCHAR(80),
+    serie VARCHAR(5)
+);
+
+CREATE TABLE IF NOT EXISTS cliente_contacto (
+    id SERIAL PRIMARY KEY,
+    id_cliente INT REFERENCES clientes(id) ON DELETE CASCADE,
+    nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(100),
+    telefono VARCHAR(20)
+);
+
+CREATE TABLE IF NOT EXISTS ventas (
+    COT NUMERIC(5, 0) PRIMARY KEY,
+    id_cliente INT REFERENCES clientes(id) ON DELETE CASCADE,
+    fecha_creacion TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    estado NUMERIC(1, 0) NOT NULL,
+    fecha_cot DATE,
+    orden_de_compra NUMERIC(12, 0),
+    fecha_oco DATE,
+    nota_de_venta NUMERIC(5, 0)
+);
+
+CREATE TABLE IF NOT EXISTS venta_detalle (
+    id_venta NUMERIC(5, 0) REFERENCES ventas(COT) ON DELETE CASCADE,
+    id_producto INT REFERENCES productos(id) ON DELETE CASCADE,
+    cantidad INT NOT NULL,
+    precio_unitario INT NOT NULL,
+    PRIMARY KEY (id_venta, id_producto)
+);
+
+CREATE TABLE IF NOT EXISTS facturas (
+    id SERIAL PRIMARY KEY,
+    id_venta NUMERIC(5, 0) REFERENCES ventas(COT) ON DELETE CASCADE,
+    fecha_emision DATE NOT NULL,
+    estado NUMERIC(1, 0) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS factura_detalle (
+    id_factura INT REFERENCES facturas(id) ON DELETE CASCADE,
+    id_producto INT REFERENCES productos(id) ON DELETE CASCADE,
+    cantidad INT NOT NULL,
+    PRIMARY KEY (id_factura, id_producto)
+);
+
+CREATE TABLE IF NOT EXISTS guia_despacho (
+    numero_guia NUMERIC(4, 0) PRIMARY KEY,
+    id_venta NUMERIC(5, 0) REFERENCES ventas(COT) ON DELETE CASCADE,
+    fecha_despacho DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS guia_detalle (
+    id_guia INT REFERENCES guia_despacho(numero_guia) ON DELETE CASCADE,
+    id_producto INT REFERENCES productos(id) ON DELETE CASCADE,
+    cantidad INT NOT NULL,
+    PRIMARY KEY (id_guia, id_producto)
+);
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# ProyectoArqSoftware
-Repositorio para nuestro proyecto de Arquitectura de Software. Integrantes: Diego Banda, Branco Burotto, Valentína Garcia, Diego Salazar
+# Proyecto Arquitectura de Software: 
+Repositorio para nuestro proyecto de Arquitectura de Software.
+
+### Integrantes
+- Diego Banda ([DiegoBan](https://github.com/DiegoBan))
+- Branco Burotto ([branxeto](https://github.com/branxeto))
+- Valentína Garcia ([balentula](https://github.com/balentula))
+- Diego Salazar ([HoHenHeimpepsi](https://github.com/HoHenHeimpepsi))
+
+## Sobre el proyecto
+Software creado con el objetivo de eficientizar, y coordinar registro de ventas, facturación, clientes, etc. Se utiliza la arquitectura SOA, con un bus central que gestiona la comunicación entre el cliente de la aplicación y los distintos servicios encargados de la lógica, cálculos, guardado de registros en base de datos, etc.
+
+### Cómo levantar proyecto
+Los distintos servicios están creados para ser contenidos en Docker, cada uno con su propio contenedor, de esta manera se mantiene el ambiente correctamente separado para cada uno.
+
+Para levantar el proyecto se utiliza el siguiente comando en la carpeta principal del proyecto:
+```
+docker compose up --build -d
+```
+
+### Base de Datos
+Como base de datos utilizamos postgreSQL, ya que gracias a su modelo relacional y estructura permite crear las relaciones necesarias sin problemas a la vez que se mantienen los datos consistentemente, cumpliendo con las propiedades ACID (Atomicidad, Consistencia, Aislamiento y Durabilidad), algo de suma importancia para un software al que no se le puede pasar por alto nada.
+<p align="center">
+    <img src=".DB/documentacion/entidad_relacion.png" alt="entidad-relacion" />
+</p>
+<p align="center">
+    <img src=".DB/documentacion/modelo_relacional.png" alt="modelo relacional" />
+</p>
