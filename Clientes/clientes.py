875a20ff9f06aaf94a38812b72d879c262316b4a
branxeto <brancoburotto@gmail.com>
2026-06-02 01:33:50 -0400

    Creación de servicio Clientes con su respectivo contenedor y funciones creadas de (Obtener clientes, Actualizar clientes)

---
+++ Clientes/clientes.py
@@ -0,0 +1,47 @@
+from soa_lib import connect_to_bus, send_message, receive_message
+from functions import dbconnect, obtener_clientes, actualizar_cliente
+import json
+
+sock = connect_to_bus()
+
+try:
+    # Registro inicial (sinit)
+    print("Registrando servicio 'clien'...")
+    send_message(sock, "sinit", "clien")    #   Nombre de servicio en bus usuar
+    
+    # Procesar respuesta del sinit
+    init_data = receive_message(sock)
+    print(f"Confirmación de bus recibida: {init_data!r}")
+    print("Servicio listo para recibir transacciones.\n")
+    
+    # Conectarse a la base de datos
+    print("Conectando con base de datos...")
+    db = dbconnect()
+    if db is None:
+        exit()
+    # Bucle principal de trabajo
+    print("Entrando en bucle principal de trabajo...")
+    while True:
+        data = receive_message(sock)
+        if not data:
+            print("Conexión cerrada por el bus.")
+            break
+        
+        mensaje = data[5:].decode()
+        datos_json = json.loads(mensaje)
+        
+        accion = datos_json.get("accion")
+        print(f"Realizando acción: '{accion}'")
+        match accion:
+            case "obtener_clientes":
+                datos_json = obtener_clientes(db)
+                send_message(sock, "clien", datos_json)
+            case "actualizar_cliente":
+                datos_json = actualizar_cliente(db, datos_json)
+                send_message(sock, "clien", datos_json)
+
+except Exception as e:
+    print(f"Error en el servicio: {e}")
+finally:
+    print('Cerrando socket del servicio')
+    sock.close()
---
+++ Clientes/functions.py
@@ -0,0 +1,104 @@
+import json
+import os
+import psycopg2
+from psycopg2 import Error
+
+def dbconnect():
+    try:
+        # Obtener credenciales
+        connection = psycopg2.connect(
+            host=os.environ.get("DB_HOST", "db"),
+            database=os.environ.get("DB_NAME", "db"),
+            user=os.environ.get("DB_USER", "admin"),
+            password=os.environ.get("DB_PASS", "colocolo"),
+            port="5432"
+        )
+        cursor = connection.cursor()
+        print("Conexion exitosa")
+        return cursor
+    except (Exception, Error) as error:
+        print("Error al conectar con postgreSQL:", error)
+        return None
+    
+def obtener_clientes(db):
+    print("<--- Obteniendo usuarios --->")
+    query = """
+        SELECT * FROM clientes;
+    """
+    try:
+        db.execute(query)
+        clientes = db.fetchall()
+        if not clientes:
+            print("No existen Clientes")
+            return json.dumps({
+                "estado": "error",
+                "mensaje": "No existen clientes"
+            })
+        else:
+            print("Se han obtenido clientes")
+            
+            nombres_columnas = [columna[0] for columna in db.description]
+            clientes_formateados = [dict(zip(nombres_columnas, filas)) for filas in clientes]
+            
+            return json.dumps({
+                "estado": "ok",
+                "mensaje": "Clientes obtenidos",
+                "clientes": clientes_formateados
+            })
+    except Exception as e:
+        print(f"Error en la obtención de clientes: {e}")
+        return json.dumps({
+            "estado": "error",
+            "mensaje": "error interno del servidor"
+        })
+    
+def actualizar_cliente(db, datos_json):
+    print("<--- Actualizando cliente --->")
+    print("Datos a actualizar:", datos_json)
+    
+    verify = """
+        SELECT rut FROM usuarios 
+        WHERE rol = 'admin';
+    """
+    query = """
+        UPDATE cliente
+        SET nombre=%s, rut_empresa=%s
+        WHERE id=%s;
+    """
+    
+    try:
+        db.execute(verify)
+        admins = db.fetchall()
+        admins = [fila[0] for fila in admins]
+        if datos_json["user"] not in admins: # Los usuarios que se tienen que verificar se tiene que arreglar mas adelante
+            return json.dumps({
+                "estado": "error",
+                "tipo": "Usuario no verificado",
+                "mensaje": "No se pudo actualizar el cliente porque el usuario no es administrador.",
+                "detalles": {
+                    "User": "El usuario no es administrador"
+                }
+            })
+        else:
+            nombre = datos_json.get("nombre")
+            rut_empresa = datos_json.get("rut_empresa")
+            id = datos_json.get("id")
+            
+            db.execute(query, (nombre, rut_empresa, id))
+            db.connection.commit()
+            print('Query enviada a base de datos, esperando respuesta...')
+            
+            return json.dumps({
+                "estado": "ok",
+                "mensaje": "Actualización exitosa",
+                "detalles": {
+                    "nombre": nombre,
+                    "rut_empresa": rut_empresa
+                }
+            })
+    except Exception as e:
+        print(f"Error en actualización de cliente: {e}")
+        return json.dumps({
+            "estado": "error",
+            "mensaje": "error interno del servidor"
+        })
